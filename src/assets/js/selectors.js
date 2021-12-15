27071ba7dbb92107edcbc47791f6fd5991b22ae8
movie42 <44064122+movie42@users.noreply.github.com>
2021-12-15 16:55:19 +0900

    selector, noticeEdit change design

--- src/assets/js/selectors.js
+++ src/assets/js/selectors.js
@@ -10,3 +10,14 @@ export const sendButton = getSelector(".send_btn");
 // container
 export const editor = getSelector("#editor");
 export const menuContainer = getSelector(".menu_block");
+
+// editor
+export const editorContainer = getSelector("#editor");
+export const viewContainer = getSelector("#viewer");
+
+export const editorTitle = getSelector(
+  ".form_container.editor_container form input[name='title']"
+);
+export const editorCheckbox = getSelector(
+  ".form_container.editor_container form div input[name='isWeekly']"
+);
