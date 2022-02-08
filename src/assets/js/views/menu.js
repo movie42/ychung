7b9afcfaae41b4e3f85b45257f03ca1c96d055f9
movie42 <44064122+movie42@users.noreply.github.com>
2022-02-08 22:43:13 +0900

    menu.js class로 변경

--- src/assets/js/views/menu.js
+++ src/assets/js/views/menu.js
@@ -1,98 +1,124 @@
 import { $ } from "../utils/utils";
+import { eventTrigger, windowEventTrigger } from "../events/event";
+import { _filter } from "../utils/helperFunctions";
 
-let prevHeight = 0;
-let top = hideMenuContainer.style.top;
-
-export const handleOpenMenu = (event) => {
-  if (event.key === "Enter" || event.type === "click")
-    menuContainer.classList.add("block");
-};
-
-export const handleCloseMenu = (event) => {
-  if (event.key === "Enter" || event.type === "click")
-    menuContainer.classList.remove("block");
-};
-
-export function handleHiddenMenu() {
-  let scrollHeight = window.scrollY;
-  let remain = prevHeight - scrollHeight;
-
-  if (scrollHeight < 60) {
-    hideMenuContainer.classList.add("hidden");
-    hideMenuContainer.classList.remove("active");
+class Menu {
+  constructor() {
+    this.prevHeight = 0;
+    this.menuContainer = $(".menu_block");
+    this.fixedHeaderOnTop = $("#top_fixed_header");
+    this.top = this.fixedHeaderOnTop.style.top;
+    this.init();
   }
 
-  // up
-  if (remain >= 0 && top < 0) {
-    top += remain;
-    if (top > 0) {
-      top = 0;
-    }
-    hideMenuContainer.classList.remove("hidden");
-    hideMenuContainer.classList.add("active");
-    hideMenuContainer.style.top = `${top}px`;
-  }
-
-  // down
-  if (remain <= 0 && top > -76 && scrollHeight > 80) {
-    top -= -remain;
-    if (top < -76) {
-      top = -76;
-    }
-    hideMenuContainer.classList.remove("hidden");
-    hideMenuContainer.style.top = `${top}px`;
-  } else if (top < -76) {
-    top = -76;
-  }
-  prevHeight = scrollHeight;
-}
-
-export function sideMenuHandler() {
-  const viewParagraphContainer = $(".toastui-editor-contents");
-  const width = window.innerWidth;
-
-  function beMenu(className) {
-    return $(className) !== null;
-  }
-
-  function createMenu(node) {
-    const tagList = viewParagraphContainer.childNodes;
-    const titleList = _filter(
-      tagList,
-      (node) =>
-        node.nodeName !== "#text" &&
-        node.nodeName !== "P" &&
-        node.nodeName !== "UL",
+  init() {
+    eventTrigger("#menu_button", this.showMenuBlock, "click", "keydown");
+    eventTrigger(".close_button", this.showMenuBlock, "click", "keydown");
+    eventTrigger(
+      "#menu_button_hide_menu",
+      this.showMenuBlock,
+      "click",
+      "keydown"
     );
+    windowEventTrigger(
+      "#top_fixed_header",
+      this.controlHiddenHeader,
+      "scroll",
+      "load"
+    );
+    windowEventTrigger(".side_menu", this.sideMenuHandler, "load", "resize");
+  }
 
-    const listContainer = document.createElement("ul");
-    node.append(listContainer);
+  showMenuBlock = (event) => {
+    if (event.key === "Enter" || event.type === "click") {
+      this.menuContainer.classList.toggle("block");
+    }
+  };
 
-    for (let i = 0; i < titleList.length; i++) {
-      const list = document.createElement("li");
-      const link = document.createElement("a");
-      titleList[i].id = `title${i}`;
-      const textValue = titleList[i].innerText;
-      link.innerText = textValue.length < 10 ? textValue : `${textValue}`;
-      link.setAttribute("href", `#${titleList[i].id}`);
+  controlHiddenHeader = () => {
+    let scrollHeight = window.scrollY;
+    let remain = this.prevHeight - scrollHeight;
 
-      list.append(link);
-      listContainer.append(list);
+    if (scrollHeight < 60) {
+      this.fixedHeaderOnTop.classList.add("hidden");
+      this.fixedHeaderOnTop.classList.remove("active");
+    }
+
+    // up
+    if (remain >= 0 && this.top < 0) {
+      this.top += remain;
+      if (this.top > 0) {
+        this.top = 0;
+      }
+      this.fixedHeaderOnTop.classList.remove("hidden");
+      this.fixedHeaderOnTop.classList.add("active");
+      this.fixedHeaderOnTop.style.top = `${this.top}px`;
+    }
+
+    // down
+    if (remain <= 0 && this.top > -76 && scrollHeight > 80) {
+      this.top -= -remain;
+      if (this.top < -76) {
+        this.top = -76;
+      }
+      this.fixedHeaderOnTop.classList.remove("hidden");
+      this.fixedHeaderOnTop.style.top = `${this.top}px`;
+    } else if (this.top < -76) {
+      this.top = -76;
+    }
+    this.prevHeight = scrollHeight;
+  };
+
+  sideMenuHandler() {
+    console.log("hi");
+    const viewParagraphContainer = $(".toastui-editor-contents");
+    const width = window.innerWidth;
+
+    function beMenu(className) {
+      return $(className) !== null;
+    }
+
+    function createMenu(node) {
+      const tagList = viewParagraphContainer.childNodes;
+      const titleList = _filter(
+        tagList,
+        (node) =>
+          node.nodeName !== "#text" &&
+          node.nodeName !== "P" &&
+          node.nodeName !== "UL"
+      );
+
+      const listContainer = document.createElement("ul");
+      node.append(listContainer);
+
+      for (let i = 0; i < titleList.length; i++) {
+        const list = document.createElement("li");
+        const link = document.createElement("a");
+        titleList[i].id = `title${i}`;
+        const textValue = titleList[i].innerText;
+        link.innerText = textValue.length < 10 ? textValue : `${textValue}`;
+        link.setAttribute("href", `#${titleList[i].id}`);
+
+        list.append(link);
+        listContainer.append(list);
+      }
+    }
+
+    const sideMenu = beMenu(".side_menu");
+
+    if (ComponentWrapper !== null && width > 1200 && !sideMenu) {
+      const sideMenuContainer = document.createElement("div");
+      ComponentWrapper.prepend(sideMenuContainer);
+
+      sideMenuContainer.classList.add("side_menu");
+      sideMenuContainer.classList.add("active");
+
+      createMenu(sideMenuContainer);
+    } else if (width < 1200 && sideMenu) {
+      const sideMenuContainer = getSelector(".side_menu");
+      sideMenuContainer.remove();
     }
   }
-
-  const sideMenu = beMenu(".side_menu");
-
-  if (ComponentWrapper !== null && width > 1200 && !sideMenu) {
-    const sideMenuContainer = document.createElement("div");
-    ComponentWrapper.prepend(sideMenuContainer);
-
-    sideMenuContainer.classList.add("side_menu");
-    sideMenuContainer.classList.add("active");
-
-    createMenu(sideMenuContainer);
-  } else if (width < 1200 && sideMenu) {
-    const sideMenuContainer = getSelector(".side_menu");
-    sideMenuContainer.remove();
-  }
 }
+
+export default Menu;
